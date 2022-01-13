3426701be812106bf06dfd65e8a227ea53f897ea
Goodness <goodnessokoye10@gmail.com>
2022-01-13 12:04:40 +0100

    initial

---
+++ home.js
@@ -0,0 +1,37 @@
+const calories = document.getElementById("InputCalorie");
+const findMeal = document.getElementById("CalorieButton");
+const mealList = document.getElementById("MealList");
+
+
+let MealArray=[];
+
+// findMeal.addEventListener("click", (e)=>{
+//     e.preventDefault();
+
+//     let Meal = localStorage.getItem("Meal");
+// })
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
